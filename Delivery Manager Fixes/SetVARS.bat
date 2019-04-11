5bbb76ad8b02355eeb77f682a00154c6aec6a9c7
Bryan Smith <bryan.smith.dev@gmail.com>
2019-04-11 15:10:59 -0400

    remove unintentional line

--- Delivery Manager Fixes/SetVARS.bat
+++ Delivery Manager Fixes/SetVARS.bat
@@ -3,5 +3,4 @@ set "callDir=%~dp0"
 set "basePath=%~dp0..\..\"
 set "repoDir=%~dp0..\"
 set "gitDir=%~dp0..\Misc\git\bin\"
-if "%myrace1PVScripts%"=="" setx myrace1PVScripts %callDir%
 setx myrace1PVScripts "%callDir%\"
