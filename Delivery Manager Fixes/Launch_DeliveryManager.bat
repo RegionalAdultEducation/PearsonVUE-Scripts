e83f4629f06eeb1b651249694cfe7e264f26001f
Bryan Smith <bryan.smith.dev@gmail.com>
2019-04-11 10:25:26 -0400

    Lets not exit if there is an error during 'fixes' stage
    So that delivery manager will still attempt to run.

--- Delivery Manager Fixes/Launch_DeliveryManager.bat
+++ Delivery Manager Fixes/Launch_DeliveryManager.bat
@@ -1,6 +1,6 @@
 echo ----- Running Fixes
 call "Fix Visible Applications Error.bat" >nul 2>&1
-IF %ERRORLEVEL% NEQ 0 ( echo. && echo /\/\/\/\ ERROR: %ERRORLEVEL% && pause && exit /b %ERRORLEVEL%)
+IF %ERRORLEVEL% NEQ 0 ( echo. && echo /\/\/\/\ ERROR: %ERRORLEVEL% && pause)
 echo.
 echo ----- Running Delivery Manager
 start "" "%programfiles(x86)%\Pearson VUE\PreRun.exe" -f VTSDeliveryManager.lpr
