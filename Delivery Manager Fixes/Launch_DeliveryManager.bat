b843ccde4dcd88e5bdbce805a4fcad546f07e705
Bryan Smith <bryan.smith.dev@gmail.com>
2019-06-04 15:52:37 -0400

    Update Delivery Manager Launch for new PV Version 7+

--- Delivery Manager Fixes/Launch_DeliveryManager.bat
+++ Delivery Manager Fixes/Launch_DeliveryManager.bat
@@ -3,6 +3,9 @@ call "Fix Visible Applications Error.bat" >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 ( echo. && echo /\/\/\/\ ERROR: %ERRORLEVEL% && pause)
 echo.
 echo ----- Running Delivery Manager
+if exist "%programfiles(x86)%\Pearson VUE\app\profiles\deliverymanager.lpr" (
+start "" "%programfiles(x86)%\Pearson VUE\PreRun.exe" -c "%programfiles(x86)%\Pearson VUE\\"  -f deliverymanager.lpr
+) else (
 start "" "%programfiles(x86)%\Pearson VUE\PreRun.exe" -f VTSDeliveryManager.lpr
+)
 IF %ERRORLEVEL% NEQ 0 ( echo. && echo /\/\/\/\ ERROR: %ERRORLEVEL% && pause && exit /b %ERRORLEVEL%)
-
